b9fc0937155bfd767b4dac9ad4b60c9b23d502f0
juliancochran <julian.p.cochran@gmail.com>
2020-01-31 12:24:11 -0500

    Updated 1/31/2020

---
+++ venv/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'RUw2gUROglc2tOzw6Zna4O15c'
+consumer_secret = 'UuQfhCijIhMw2ndjcd5rGxEpbxKLL0BFYCvlU2alK2eXzWNhUl'
+access_token = '304613716-y5nxn4vR4nwFTgxIwtr3zF3rXZ1T9xQs5E0Rs0eg'
+access_secret = 'gxQcDRyOLjaWiYWzGRWzwcnSvgeZsaf1R25YxLOSFmN4R'
--- venv/sample_twitter.py
+++ venv/sample_twitter.py
@@ -1,10 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = 'YOUR-CONSUMER-KEY'
-consumer_secret = 'YOUR-CONSUMER-SECRET'
-access_token = 'YOUR-ACCESS-TOKEN'
-access_secret = 'YOUR-ACCESS-SECRET'
+consumer_key = 'RUw2gUROglc2tOzw6Zna4O15c'
+consumer_secret = 'UuQfhCijIhMw2ndjcd5rGxEpbxKLL0BFYCvlU2alK2eXzWNhUl'
+access_token = '304613716-PFXz6wYRkusCB7s32xaAJdBi6HlTcwya0LdgSOJo'
+access_secret = 'P65ImDw67XYmogD1cfSKIxfSoXfTlaw1mxtnIEVSzT73x'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
